81d7ba5d60776a3687561f40f25f857a5f97bbfc
Patrick <patrick.almeida06@gmail.com>
2025-10-29 12:08:03 -0300

    feat:Created my favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Gymlog",
   description: "A web app to track your gym progress",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
